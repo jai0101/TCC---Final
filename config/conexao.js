f81525e4cae02ae6511214c0a3d4a9c94c73f231
Jaisasudati <jaisasudati@gmail.com>
2025-05-03 15:10:14 -0300

    commit

--- config/conexao.js
+++ config/conexao.js
@@ -2,20 +2,22 @@ require('dotenv').config(); // Carrega as variáveis do .env
 
 const mongoose = require('mongoose');
 
-// Aqui, acessamos a variável MONGO_URI que está no .env
+// Define a opção para lidar com avisos futuros do Mongoose
+mongoose.set('strictQuery', true);
+
+// Pega a URI do MongoDB do .env
 const uri = process.env.MONGO_URI;
 
+// Verifica se a URI está definida
 if (!uri) {
-    console.error('Erro: A variável de ambiente MONGO_URI não está definida.');
-    process.exit(1); // Se a variável não existir, o processo é interrompido
+    console.error('❌ Erro: A variável de ambiente MONGO_URI não está definida.');
+    process.exit(1);
 }
 
+// Conecta ao MongoDB
 mongoose.connect(uri)
     .then(() => {
-        console.log('Conectado ao MongoDB');
+        console.log('✅ Conectado ao MongoDB com sucesso!');
     })
     .catch((error) => {
-        console.error('Erro na conexão:', error);
-    });
-
-module.exports = mongoose;
+        console.error('❌ Erro na conexã
